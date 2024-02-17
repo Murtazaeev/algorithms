931766b4ac939d8a4e94cde5621e214596f152d1
Temur <temur.murtazaeveurope@gmail.com>
2024-02-17 07:20:49 +0100

    solutions for part 1

--- ProblemSolvingPatterns/index.js
+++ ProblemSolvingPatterns/index.js
@@ -164,17 +164,106 @@ console.log(search([1, 2, 3, 4, 5, 6], 4));
 
 // BinarySearch log(n)
 
-
 //challange
 function areThereDuplicates() {
   let obj = {};
-  for(let i=0;i<arguments.length;i++){
+  for (let i = 0; i < arguments.length; i++) {
     let val = arguments[i];
-    if(!obj[val]){
+    if (!obj[val]) {
       obj[val] = 1;
     } else {
       return true;
     }
   }
   return false;
+}
+
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+// Your solution MUST have the following complexities:
+
+// Time: O(N)
+
+// Sample Input:
+
+// sameFrequency(182,281) // true
+// sameFrequency(34,14) // false
+// sameFrequency(3589578, 5879385) // true
+// sameFrequency(22,222) // false
+
+function sameFrequency(num1, num2) {
+  let strNum1 = num1.toString();
+  let strNum2 = num2.toString();
+  if (strNum1.length !== strNum2.length) return false;
+
+  let countNum1 = {};
+  let countNum2 = {};
+
+  for (let i = 0; i < strNum1.length; i++) {
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
+  }
+
+  for (let j = 0; j < strNum1.length; j++) {
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
+  }
+
+  for (let key in countNum1) {
+    if (countNum1[key] !== countNum2[key]) return false;
+  }
+
+  return true;
+}
+
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+// Restrictions:
+
+// Time - O(n)
+
+// Space - O(n)
+
+// Bonus:
+
+// Time - O(n log n)
+
+// Space - O(1)
+
+function areThereDuplicates() {
+  let collection = {};
+  for (let val in arguments) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true;
+  }
+  return false;
+}
+
+function areThereDuplicates(...args) {
+  args.sort((a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  });
+
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true;
+    }
+    start++;
+    next++;
+  }
+  return false;
+}
+
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
 }

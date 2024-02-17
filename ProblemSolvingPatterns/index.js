69889ec276d490c56612d51007153d3ebda35a0b
Temur <temur.murtazaeveurope@gmail.com>
2024-02-17 14:46:55 +0100

    Solution part 2

--- ProblemSolvingPatterns/index.js
+++ ProblemSolvingPatterns/index.js
@@ -12,7 +12,6 @@ function same(arr1, arr2) {
   return true;
 }
 
-console.log(same([1, 2, 3, 2], [9, 4, 1, 4]));
 // but its n^2 :(((
 
 // second version
@@ -24,7 +23,6 @@ function same(arr1, arr2) {
   let frequencyCounter2 = {};
 
   for (let val of arr1) {
-    console.log("hello", frequencyCounter1[val]);
     frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
   }
   for (let val of arr2) {
@@ -40,8 +38,6 @@ function same(arr1, arr2) {
   }
   return true;
 }
-
-console.log(same([1, 2, 3, 2, 5], [9, 4, 1, 4, 25]));
 
 // anagram challange
 function validAnagram(string1, string2) {
@@ -66,12 +62,8 @@ function validAnagram(string1, string2) {
       return false;
     }
   }
-  console.log(frequencyCounter1);
-  console.log(frequencyCounter2);
   return true;
 }
-
-console.log(validAnagram("anagram", "nagaram"));
 
 // sumZero function
 function sumZero(arr) {
@@ -90,8 +82,6 @@ function sumZero(arr) {
   }
 }
 
-console.log("answer: ", sumZero([-4, -3, -2, -1, 0, 1, 9, 7, 8]));
-
 // countUniqueValues: implement func which accepts sorted arr, and counts the unique vals in the arr
 // There can be negative nums in the arr, but it will always be sorted
 function countUniqueValues(arr) {
@@ -105,8 +95,6 @@ function countUniqueValues(arr) {
   }
   return uniqueVal;
 }
-
-console.log(countUniqueValues([-2, -1, -1, 0, 1]));
 
 // sliding window for time O(n^2)
 function maxSubarraySum(arr, num) {
@@ -124,8 +112,6 @@ function maxSubarraySum(arr, num) {
   }
   return maxVal;
 }
-
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 3));
 
 // sliding window for O(n)
 function maxSubarraySum(arr, numb) {
@@ -148,8 +134,6 @@ function maxSubarraySum(arr, numb) {
   return maxVal;
 }
 
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
-
 // search O(n)
 function search(arr, num) {
   for (let i = 0; i <= arr.length; i++) {
@@ -159,8 +143,6 @@ function search(arr, num) {
   }
   return -1;
 }
-
-console.log(search([1, 2, 3, 4, 5, 6], 4));
 
 // BinarySearch log(n)
 
@@ -178,14 +160,11 @@ function areThereDuplicates() {
   return false;
 }
 
-// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
-
+// Write a function called sameFrequency. Given two positive integers,
+// find out if the two numbers have the same frequency of digits.
 // Your solution MUST have the following complexities:
-
 // Time: O(N)
-
 // Sample Input:
-
 // sameFrequency(182,281) // true
 // sameFrequency(34,14) // false
 // sameFrequency(3589578, 5879385) // true
@@ -200,7 +179,11 @@ function sameFrequency(num1, num2) {
   let countNum2 = {};
 
   for (let i = 0; i < strNum1.length; i++) {
+    console.log("hello1", strNum1[i]);
+    console.log("hello2", countNum1[strNum1[i]]);
+    console.log("hello3", (countNum1[strNum1[i]] || 0) + 1);
     countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
+    console.log("hello4", countNum1);
   }
 
   for (let j = 0; j < strNum1.length; j++) {
@@ -213,25 +196,19 @@ function sameFrequency(num1, num2) {
 
   return true;
 }
+sameFrequency(12228, 28221);
 
 // Frequency Counter / Multiple Pointers - areThereDuplicates
 // Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
-
 // Examples:
-
 // areThereDuplicates(1, 2, 3) // false
 // areThereDuplicates(1, 2, 2) // true
 // areThereDuplicates('a', 'b', 'c', 'a') // true
 // Restrictions:
-
 // Time - O(n)
-
 // Space - O(n)
-
 // Bonus:
-
 // Time - O(n log n)
-
 // Space - O(1)
 
 function areThereDuplicates() {
@@ -244,6 +221,7 @@ function areThereDuplicates() {
   }
   return false;
 }
+areThereDuplicates(1, 2, 3);
 
 function areThereDuplicates(...args) {
   args.sort((a, b) => {
@@ -266,4 +244,72 @@ function areThereDuplicates(...args) {
 
 function areThereDuplicates() {
   return new Set(arguments).size !== arguments.length;
+}
+
+// SOLUTION PART 2
+
+//Multiple Pointers - averagePair
+// Write a function called averagePair. Given a sorted array of integers
+// and a target average, determine if there is a pair of values in the array
+//  where the average of the pair equals the target average. There may
+//  be more than one pair that matches the average target.
+
+// Bonus Constraints:
+// Time: O(N)
+// Space: O(1)
+// Sample Input:
+
+// averagePair([1,2,3],2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
+
+function averagePair(arr, num) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2;
+    if (avg === num) return true;
+    else if (avg < num) start++;
+    else end--;
+  }
+  return false;
+}
+
+// Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings
+// and checks whether the characters in the first string form a subsequence
+// of the characters in the second string. In other words, the function should check
+//  whether the characters in the first string appear somewhere in the second string,
+// without their order changing.
+
+// Examples:
+
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+// Your solution MUST have AT LEAST the following complexities:
+// Time Complexity - O(N + M)
+// Space Complexity - O(1)
+
+//iterative
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+//Recursive but not O(1) Space
+function isSubsequence(str1, str2) {
+  if (str1.length === 0) return true;
+  if (str2.length === 0) return false;
+  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+  return isSubsequence(str1, str2.slice(1));
 }

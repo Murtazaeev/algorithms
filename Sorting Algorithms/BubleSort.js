60b219f4467713a9e1330a2a7e443b129d47aa33
Temur <temur.murtazaeveurope@gmail.com>
2024-03-02 15:54:40 +0100

    bubleSort

---
+++ Sorting Algorithms/BubleSort.js
@@ -0,0 +1,15 @@
+function bubleSort(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+bubleSort([37, 45, 12, 49]);
